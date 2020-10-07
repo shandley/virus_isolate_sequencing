263a2f360243418296963e8070926b9ecb5b6fdd
shandley <handley.scott@gmail.com>
2020-10-07 12:23:46 -0500

    Added command to include allele frequencies in final table

--- compile_annotated_vcf.sh
+++ compile_annotated_vcf.sh
@@ -17,11 +17,14 @@ for i in *.vcf; do
 
 	sed -i '1iVARIANT_TYPE\tIMPACT\tORF_GENE\tORF_COORDINATES\tORF_ACC\tORF_TYPE\tVARIANT_AA_CHANGE\tORF_PROTEIN' "$F".second;
 
-	paste "$F".first "$F".second > $OUT/"$F".snpEFF_ann.txt; # Save as text file for easier sharing with collaborators (can be easily opened/imported into Excel) 
+	grep -v "#" "$F".vcf | cut -f8 | awk -F ";" '{ print$2 }' > "$F".af;
+
+	paste "$F".first "$F".af "$F".second > $OUT/"$F".snpEFF_ann.txt; 
 
 
 ## Clean up
 	rm *.first;
 	rm *.second;
+	rm *.af
 
 done
